f2e16a37042c9dcda74a206dba338e7c56e8bc2d
David Gene <davidgene695@gmail.com>
2026-02-18 17:43:04 +0000

    Manual code change

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
-<!-- Ethers.js from CDN -->
-<><script src="https://cdn.ethers.io/lib/ethers-5.7.2.umd.min.js" type="application/javascript"></script>< /></>!-- Axios from CDN -->
-<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
