6dee42f06b62d552ebb598ea49792c597a1b8b77
David Gene <davidgene695@gmail.com>
2026-02-18 16:54:16 +0000

    Manual code change

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,3 @@ import '@/index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 )
-<!-- Ethers.js from CDN -->
-<script src="https://cdn.ethers.io/lib/ethers-5.7.2.umd.min.js" type="application/javascript"></script>
-
-<!-- Axios from CDN -->
-<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
