d6c2b2c8df400880c63c710b4d95aacf20c66941
davidgene659-cloud <davidgene659@gmail.com>
2026-02-19 18:15:48 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { Buffer } from "buffer";
 globalThis.Buffer = Buffer;
 
-import App from "./App";
-import "./index.css";
-// ... rest of your entry file unchangedimport React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '@/App.jsx';
+import '@/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
