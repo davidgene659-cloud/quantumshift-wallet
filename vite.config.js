05aa00881421eb322b79cf83ecfa5c3719aa4c37
davidgene659-cloud <davidgene659@gmail.com>
2026-02-19 18:13:13 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,12 +18,19 @@ export default defineConfig({
   ]
 });
 import { defineConfig } from "vite";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    nodePolyfills({
-      include: ["buffer", "process", "stream", "util"],
-    }),
-  ],
+  plugins: [react()],
+  define: {
+    global: "globalThis",
+  },
+  resolve: {
+    alias: {
+      buffer: "buffer",
+    },
+  },
+  optimizeDeps: {
+    include: ["buffer"],
+  },
 });
