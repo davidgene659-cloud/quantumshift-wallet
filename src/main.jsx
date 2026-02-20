84a613490b4cacadc7852d77eac96dc35b4cb257
davidgene659-cloud <davidgene659@gmail.com>
2026-02-19 18:11:51 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import { Buffer } from "buffer";
+globalThis.Buffer = Buffer;
+
+import App from "./App";
+import "./index.css";
+// ... rest of your entry file unchangedimport React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
